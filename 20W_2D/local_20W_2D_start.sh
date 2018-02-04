3490f91ac85e9d103359fc567dbd833c7fb7c59f
Kevin <kgvansly@buffalo.edu>
2018-02-04 12:30:51 -0500

    Removed instance of created files

--- 20W_2D/local_20W_2D_start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-echo "Launching molecular dynamics filtration simulation(s)..." 
-echo "Running mpirun -n 2 /usr/local/LAMMPS/src/lmp_auto -nocite -in input_20W_2D_start.lmp -log log_20W_2D_start.lmp" 
-mpirun -n 2 /usr/local/LAMMPS/src/lmp_auto -nocite -in input_20W_2D_start.lmp -log log_20W_2D_start.lmp 
-echo "All Done!" 
