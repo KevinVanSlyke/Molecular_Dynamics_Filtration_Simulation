82e8b7b17f8f039e436c9b4788c8cf7e967dc23b
Kevin G. Van Slyke <kgvansly@buffalo.edu>
2021-04-27 08:20:54 -0400

    removed depricated
    needed to push without the directory for gitignore to kick in

--- Depricated/MATLAB/return_trialString.m
+++
@@ -1,9 +0,0 @@
-function [ trialString ] = return_trialString(logFileString)
-
-logFileParts = strsplit(logFileString,'.');
-logParts = strsplit(logFileParts{1,1},'_');
-nLogParts = size(logParts,2);
-trialPart = logParts(nLogParts-1);
-trialString = trialPart{1,1};
-
-end
--- Depricated/MATLAB/tau_parameter_plot.m
+++
@@ -1,7 +0,0 @@
-function [] = tau_parameter_plot(tauAvgs, tauStds, parNames, parVals, tauPlot)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
