4ed09fe3a18562a6e7f55ff2a0f832b27473fae6
Kevin G. Van Slyke <kgvansly@buffalo.edu>
2021-04-27 09:08:17 -0400

    deprecated
    spelled bad

--- MATLAB/depricated/collate_vcm_data.m
+++
@@ -1,6 +0,0 @@
-function [] = collate_vcm_data()
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-end
-
--- MATLAB/depricated/mesh_convert/savetofile.m
+++
@@ -1,3 +0,0 @@
-function savetofile(fullfilename,t,x,y,meshData)
-    save(fullfilename,'t','x','y','meshData');
-end
